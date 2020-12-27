540f2d3986d89e6f0931254bf3980f70a73c9d82
Carmel Shupak <carmels@wix.com>
2020-12-27 23:03:08 +0200

    add dependency

--- api/src/controllers/user.controller.ts
+++ api/src/controllers/user.controller.ts
@@ -1,18 +1,35 @@
 import { Request, Response } from "express";
 
 
-export const create = async (req: Request, res: Response): Promise<void> => {
-    res.render("user", { title: "amir" });
-};
+
 
 export const read = async (req: Request, res: Response): Promise<void> => {
-    res.render("user", { title: "amir" });
+    res.send({ username: os.userInfo().username, 
+               method: "get"})
 };
+
+export const create = async (req: Request, res: Response): Promise<any> => {
+    
+    try {
+       return res.json(({ username: gerUserName(),
+            method: "post"})).send(200)
+    } catch (error) {
+        console.log("here111",error);
+       return res.sendStatus(500)
+    }
+
+};
+
+function gerUserName(){
+    throw new Error("here error");
+    return os.userInfo().username;
+}
 
 export const update = async (req: Request, res: Response): Promise<void> => {
-    res.render("user", { title: "amir" });
-};
+    res.send({ username: os.userInfo().username, 
+        method: "put"})};
 
 export const remove = async (req: Request, res: Response): Promise<void> => {
-    res.render("user", { title: "amir" });
+    res.send({ username: os.userInfo().username, 
+        method: "delete"})    
 };
