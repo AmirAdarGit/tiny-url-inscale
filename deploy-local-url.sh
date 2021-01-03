f904a9f6a95c431d075d49890f815e945332fda6
amir adar <44618095+amiradar12345@users.noreply.github.com>
2021-01-03 20:34:02 +0200

    try again 7

--- deploy-local-url.sh
+++ deploy-local-url.sh
@@ -20,3 +20,4 @@ docker build -t amiradar/tiny-url.user-service .
 docker container rm tiny-url.user-service -f
 docker run -d -p 3001:8080 -d --name=tiny-url.user-service amiradar/tiny-url.user-service
 
+# docker run -p 8080:3000 -it --name=tiny-url.url-service amiradar/tiny-url.url-service bash
