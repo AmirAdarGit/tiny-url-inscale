885ead0625ba38f3675a938d4d002811125f830f
amir adar <44618095+amiradar12345@users.noreply.github.com>
2021-01-05 15:08:17 +0200

    change the user service container port from 3001 to 4000

--- deploy-local-url.sh
+++ deploy-local-url.sh
@@ -18,6 +18,6 @@ cd ../userService
 tsc
 docker build -t amiradar/tiny-url.user-service .
 docker container rm tiny-url.user-service -f
-docker run -d -p 3001:8080 -d --name=tiny-url.user-service amiradar/tiny-url.user-service
+docker run -d -p 4000:8080 -d --name=tiny-url.user-service amiradar/tiny-url.user-service
 
 # docker run -p 8080:3000 -it --name=tiny-url.url-service amiradar/tiny-url.url-service bash
