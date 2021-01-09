e950b2071922dd18e882dd035515dfb3dd00e96b
amir adar <44618095+amiradar12345@users.noreply.github.com>
2021-01-09 14:58:46 +0200

    delete unnecessary code...

--- urlService/src/app.ts
+++ urlService/src/app.ts
@@ -1,11 +1,9 @@
 import * as express from "express"
 
 import { router as urlEntity} from './routes/urlService.route'
-import { router as urlShortGenerator} from './routes/urlService.genetator'
 
 export const app = express();
 
 
 
 app.use('/api/url', urlEntity);
-// app.use('/api/url/shoter', urlShortGenerator);
